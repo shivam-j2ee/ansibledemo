d86a14c87777ce9c198188f0d2df1bca359c8f55
shivam-j2ee <tiwari.shivam26@gmail.com>
2022-08-06 15:51:05 +0530

    fc

---
+++ src/main/java/com/siplilearn/ansibledemo/AnsibledemoApplication.java
@@ -0,0 +1,13 @@
+package com.siplilearn.ansibledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibledemoApplication.class, args);
+	}
+
+}
